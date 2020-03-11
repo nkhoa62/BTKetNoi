6066aa1268041032655154474ebab81e0be094fe
NEWBIE <NEWBIE@DESKTOP-FPO04BE>
2020-03-11 11:07:12 +0700

    Add project files.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+
+            {
+
+                using (SqlConnection connection =
+
+                    new SqlConnection(@"Server=DESKTOP-FPO04BE\SQLEXPRESS;
+ 
+  Database=Quanlysinhvien; Integrated Security=SSPI"))
+
+                {
+
+                    connection.Open();
+
+                }
+
+                MessageBox.Show("Mo va dong co so du lieu thanh cong.");
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                MessageBox.Show("Loi khi mo  ket noi: " + ex.Message);
+
+            }
+        }
+    }
+}
